6e5c42473d5c72eb1db54d39a02e372aa8d63bba
Hsuan-Hsueh Huang <ben0630882@gmail.com>
2021-05-15 19:34:48 +0800

    finish shoppage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
+import {HashRouter,Switch,Route} from 'react-router-dom';
 import './App.css';
-import {Homepage} from './pages/homepage/homepage.component.jsx'
+import Homepage from './pages/homepage/homepage.component.jsx';
+import Shoppage from './pages/shoppage/shoppage.component.jsx';
+
 function App() {
-  
   return (
-    <div className="App">
-      <Homepage/>
+    <div>
+      <HashRouter>   
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop' component={Shoppage}/>
+        
+      </HashRouter>
     </div>
+    
   );
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './homepage.styles.scss';
 import {Dir} from '../../components/directory/directory.component.jsx';
 
-export const Homepage = (props) => {
+const Homepage = (props) => {
     //usage:
     return  <div className='homepage'>
                 <Dir/>
-            </div>
+            </div>;
 }
+export default Homepage
