83596b7cb4fba19b97f5bb1d6f60d7f438ce3473
Hsuan-Hsueh Huang <ben0630882@gmail.com>
2021-05-15 22:34:39 +0800

    finish header(navigation)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import {HashRouter,Switch,Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
 import Shoppage from './pages/shoppage/shoppage.component.jsx';
-
+import Header from './components/header/header.component.jsx'
 function App() {
   return (
     <div>
       <HashRouter>   
-        <Route exact path='/' component={Homepage}/>
-        <Route exact path='/shop' component={Shoppage}/>
-        
+        <Header/>
+        <switch>
+          <Route exact path='/' component={Homepage}/>
+          <Route exact path='/shop' component={Shoppage}/>
+        </switch>
       </HashRouter>
     </div>
     
